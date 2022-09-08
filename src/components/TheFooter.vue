fd852d401bb5c6494c137473bf0f74eeec81fb90
gomxx2 <gomgom223@gmail.com>
2022-09-08 11:10:53 +0700

    first push this project

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-footer class="pa-3">
+    GomgomBalutaro
+    <v-icon small color="red">fas fa-key</v-icon>&nbspand&nbsp
+    <v-icon small color="green">fas fa-check</v-icon>
+    <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,200 @@
+<template>
+  <v-layout column justify-center align-center class="mt-4 pt-2">
+    <VueCompareImage
+      class="hidden-md-and-down"
+      hover
+      :style="{ minWidth: '1300px' }"
+      :sliderLineWidth="sliderLine"
+      :handleSize="hSize"
+      :leftImage="leftImage"
+      :rightImage="rightImage"
+      :sliderPositionPercentage="sliderPosition"
+    />
+    <VueCompareImage
+      class="hidden-lg-and-up"
+      hover
+      :style="{ maxWidth: '400px' }"
+      :sliderLineWidth="sliderLine"
+      :handleSize="hSize"
+      :leftImage="leftImage2"
+      :rightImage="rightImage2"
+      :sliderPositionPercentage="sliderPosition"
+    />
+
+    <br>
+
+    <vue-typer class="headline" :repeat="0" text=" Gomgom Sihotang"></vue-typer>
+    <vue-typer
+      :text="text1"
+      :repeat="Infinity"
+      :shuffle="false"
+      initial-action="erasing"
+      :pre-type-delay="70"
+      :type-delay="70"
+      :pre-erase-delay="1980"
+      :erase-delay="150"
+      erase-style="select-back"
+      :erase-on-complete="false"
+      caret-animation="smooth"
+    ></vue-typer>
+    <br>
+    <v-flex xs12 sm6 md4 lg4 xl4>
+      <v-card flat color="transparent" max-width="500">
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">
+              <span>About</span>
+              <span class="green--text">Me</span>
+            </h3>
+            <div>
+              <p>
+                halo guys gua Gomgom Sihotang,
+                kesibukan gua sehari-hari bekerja di perushaan start-up menjadi 
+                <span
+                  class="green--text font-weight-bold"
+                >backend enginner</span>, selain itu juga gua menyempatkan diri untuk
+                membantu teman-teman yang ingin belajar programming. 
+                hmmm berbayar sih tapi cuman dikit kok selain itu temen-teman juga boleh 
+                nanya nanya gua kalau lagi binggung. video kan ngak bisa ditanya kan ya?
+                nah ayo join.
+              </p>
+            </div>
+          </div>
+        </v-card-title>
+
+        <!-- <v-card-actions class="hidden-sm-and-down">
+          <v-btn
+            v-for="icon in icons"
+            :key="icon.icon"
+            fab
+            dark
+            outline
+            color="green"
+            :href="icon.href"
+            target="_blank"
+          >
+            <v-icon dark>{{icon.icon}}</v-icon>
+          </v-btn>
+        </v-card-actions> -->
+
+        <v-card-actions class="hidden-md-and-up justify-center">
+          <v-btn
+            v-for="icon in icons"
+            :key="icon.icon"
+            small
+            fab
+            dark
+            outline
+            color="green"
+            :href="icon.href"
+            target="_blank"
+          >
+          <img src="../../public/img/1.png" alt="">
+            <v-icon dark>{{icon.icon}}</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import { VueTyper } from "vue-typer";
+import VueCompareImage from "vue-compare-image";
+
+export default {
+  metaInfo: {
+    title: "Home",
+    titleTemplate: "%s ← gomgom's",
+    meta: [
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "description", content: "Eldin Zaimovic's Portfolio" },
+      { charset: "utf-8" },
+      { property: "og:title", content: "Eldin' Space" },
+      { property: "og:site_name", content: "Eldin' Space" },
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: "https://eldin.space" },
+      {
+        property: "og:image",
+        content: ""
+      },
+      { property: "og:description", content: "Eldin Zaimovic's Portfolio" }
+    ]
+  },
+  components: {
+    "vue-typer": VueTyper,
+    VueCompareImage
+  },
+  data() {
+    return {
+      icons: [
+        { href: "https://github.com/EldinZaimovic", icon: "fab fa-github" },
+        {
+          href: "https://stackoverflow.com/users/story/9660700",
+          icon: "fab fa-stack-overflow"
+        },
+        { href: "https://twitter.com/EldinMatricar", icon: "fab fa-twitter" },
+        {
+          href: "https://www.linkedin.com/in/eldin-zaimovic",
+          icon: "fab fa-linkedin-in"
+        },
+        {
+          href: "https://www.xing.com/profile/Eldin_Zaimovic?sc_o=mxb_p",
+          icon: "fab fa-xing"
+        },
+        {
+          href: "https://www.facebook.com/EldinVasVoli",
+          icon: "fab fa-facebook-f"
+        },
+        { href: "https://www.instagram.com/matricar", icon: "fab fa-instagram" }
+      ],
+      text1: ["Full-Stack Developer", "Backend Engineer", "Trainer"],
+      leftImage: require("../../public/img/1.png"),
+      rightImage:require( "../../public/img/3.png"),
+      leftImage2: require("../../public/img/3.png"),
+      rightImage2: require("../../public/img/2.png"),
+      // leftImage: "https://i.xx.com/bU50uS9.jpg",
+      // rightImage: "https://i.xx.com/z3ZoEEw.jpg",
+      // leftImage2: "https://i.xx.com/z3ZoEEw.jpg",
+      // rightImage2: "https://i.xx.com/Dcz2PGx.jpg",
+      sliderLine: 0,
+      hSize: 0,
+      sliderPosition: 0.5
+    };
+  }
+};
+</script>
+<style>
+@keyframes rocking {
+  0%,
+  100% {
+    transform: rotateZ(-10deg);
+  }
+
+  50% {
+    transform: rotateZ(10deg);
+  }
+}
+
+.vue-typer {
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+}
+.vue-typer .custom.char.typed {
+  color: green;
+}
+.vue-typer .custom.char.selected {
+  color: #e91e63;
+}
+
+.vue-typer .custom.caret {
+  animation: rocking 1s ease-in-out 0s infinite;
+}
+.vue-typer .custom.caret.typing {
+  background-color: green;
+}
+.vue-typer .custom.caret.selecting {
+  display: inline-block;
+  background-color: #e91e63;
+}
+</style>
+
